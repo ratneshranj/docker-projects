66361b9e3d6f189b8990af64ff4f50e1b3777e10
HelloWorld.java <93527008+ratneshranj@users.noreply.github.com>
2022-08-05 22:07:26 +0530

    added thread

--- src/main/java/com/docker/git/HelloWorld.java
+++ src/main/java/com/docker/git/HelloWorld.java
@@ -11,10 +11,12 @@ public class HelloWorld {
 
 	/**
 	 * @param args
+	 * @throws Exception 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		for(int i=0;i<10;i++) {
 			System.out.println("Hello World "+i);
+			Thread.sleep(10000);
 		}
 	}
 
