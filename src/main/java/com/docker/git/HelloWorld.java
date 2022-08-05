e06ceffb30545949335068eeb64ee9a4ef7fee0b
HelloWorld.java <93527008+ratneshranj@users.noreply.github.com>
2022-08-05 20:14:52 +0530

    simple hello world project

---
+++ src/main/java/com/docker/git/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.docker.git;
+
+/**
+ * @author server
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		for(int i=0;i<10;i++) {
+			System.out.println("Hello World "+i);
+		}
+	}
+
+}
---
+++ src/test/java/com/docker/git/HelloWorldTest.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.docker.git;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * @author server
+ *
+ */
+public class HelloWorldTest {
+	
+	@Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+}
